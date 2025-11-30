6ebc7d837a336bf612dfd293a2464130ef64003c
c0lbalt <cheaus2@lsu.edu>
2025-11-30 15:21:43 -0600

    Configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo name is not 'username.github.io', uncomment and set basePath:
+  // basePath: '/Website_Portfolio',
+  // assetPrefix: '/Website_Portfolio',
 }
 
 module.exports = nextConfig
